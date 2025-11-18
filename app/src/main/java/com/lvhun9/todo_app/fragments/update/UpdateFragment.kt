7c01f7c19f46419f1214becfb3d8593658c6a420
Le Viet Hung <lvhung1411@gmail.com>
2025-11-19 07:46:02 +0900

    feat: Add navigation component and fragments
    Adds the Android Navigation component to the project.
    
    - Introduces `ListFragment`, `AddFragment`, and `UpdateFragment` with their basic layouts.
    - Creates a navigation graph `my_nav.xml` to define the navigation flow between these fragments.
    - Adds necessary navigation, fragment, and constraint layout dependencies.
    - Adds a placeholder string resource for blank fragments.

---
+++ app/src/main/java/com/lvhun9/todo_app/fragments/update/UpdateFragment.kt
@@ -0,0 +1,18 @@
+package com.lvhun9.todo_app.fragments.update
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lvhun9.todo_app.R
+
+class UpdateFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+}
